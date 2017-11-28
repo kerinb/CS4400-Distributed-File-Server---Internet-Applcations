6449ebacd5b43f3fbfe39b83ddc4d80d47be719b
Breandan <kerinb@tcd.ie>
2017-11-28 11:11:47 +0000

    parking create file on server until I can spawn multiple file servers

--- DirectoryServer/DirectoryServer.py
+++ DirectoryServer/DirectoryServer.py
@@ -4,7 +4,11 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+# file_server_id = IP:PORT_NUM
 ONLINE_FILE_SERVERS = {1: ('127.0.0.1', 5001)}
+# file_server_id = number of files on file server
+LOAD_ON_FILE_SERVER = {}
+# file_name = file_server_id: file_id
 LIST_OF_ALL_FILES = {'test': (1, 1)}
 
 
@@ -26,11 +30,15 @@ class DirectoryServer(Resource):
         file_id, file_server_details = get_file_details_if_exist(file_name)
         return {'file_id': file_id, 'file_server_id': file_server_details}
 
-
-    #def get(self):
-      # request = self.parser.parse_args()['request']
-    # if request == 'create':
-
+    def post(self):
+        request = self.parser.parse_args()['request']
+        if request == 'create':
+            file_name = self.parser.parse_args()['file_name']
+            file_id, file_server_details = get_file_details_if_exist(file_name)
+            if file_id is not None and file_server_details is not None:
+                return {'file_id': file_id, 'file_server_id': file_server_details}
+            print "creating a file..."
+            # come back to this when I have multiple FS's spawned...
 
 
 api.add_resource(DirectoryServer, '/')
