4072673796f87fdea4f3990d65c878217480b9c8
Breandan <kerinb@tcd.ie>
2017-12-10 16:53:36 +0000

    just added some OTT exception handling

--- Client/Cache.py
+++ Client/Cache.py
@@ -18,7 +18,7 @@ def find_LRU_key(cache_entries):
 class Cache:
     client_id = None
     client_cache_dir = ""
-    MAX_SIZE_OF_CACHE = 3  # Number of files in cache
+    MAX_SIZE_OF_CACHE = 2  # Number of files in cache  - This is actually three files, because we go from 0 - 2!
     cache_entries = []  # array of json tuples -> cache_entries[key] = [{'file': file, 'version': version}]
 
     def __init__(self, client_id):
@@ -32,96 +32,125 @@ class Cache:
         self.number_of_cache_entries = -1
 
     def initialise_cache(self, client_cache_path, client_id):
-        print "initialising cache"
-        if not os.path.exists(client_cache_path):
-            print "path to client cache does not exists... creating it"
-            os.mkdir(client_cache_path)
-            self.client_id = client_id
-            self.client_cache_dir = client_cache_path
-        else:
-            print "clients cache already exists"
+        try:
+            print "initialising cache"
+            if not os.path.exists(client_cache_path):
+                print "path to client cache does not exists... creating it"
+                os.mkdir(client_cache_path)
+                self.client_id = client_id
+                self.client_cache_dir = client_cache_path
+            else:
+                print "clients cache already exists"
+        except Exception as e:
+            print "ERROR: occurred when initialising the cache with:\n{}".format(e.message)
 
     def add_cache_entry(self, cache_file_name, version, updated_data):
-        print "adding data to cache"
+        try:
+            print "adding data to cache"
+            if cache_file_name not in self.cache_entries:
+                print "file is not in cache -add it"
+                key = self.number_of_cache_entries + 1
+                if len(self.cache_entries) is self.MAX_SIZE_OF_CACHE:
+                    print "cache is full - must use LRU function"
+                    key = self.replace_file_LRU_policy()
+                print "add file to cache here..."
+                self.cache_entries[key] = {'file': cache_file_name, 'version': version}
+                self.number_of_cache_entries += 1
+                self.update_data_in_cache(cache_file_name, updated_data)
 
-        if cache_file_name not in self.cache_entries:
-            print "file is not in cache -add it"
-            key = self.number_of_cache_entries + 1
-            if len(self.cache_entries) is self.MAX_SIZE_OF_CACHE:
-                print "cache is full - must use LRU function"
-                key = self.replace_file_LRU_policy()
-            print "add file to cache here..."
-            self.cache_entries[key] = {'file': cache_file_name, 'version': version}
-            self.number_of_cache_entries += 1
-            self.update_data_in_cache(cache_file_name, updated_data)
+            # file is in cache
+            key = self.get_key_to_file(cache_file_name)
 
-        # file is in cache
-        key = self.get_key_to_file(cache_file_name)
-
-        if version > self.cache_entries[key]['version']:
-            print "updating data in the cache"
-            print "version is not up to date - need to up date cache"
-            self.cache_entries[key] = {'file': cache_file_name, 'version': version}
-            print "updated file in cache"
-            return
-        print "file is up to date - dont need to do anything..."
+            if version > self.cache_entries[key]['version']:
+                print "updating data in the cache"
+                print "version is not up to date - need to up date cache"
+                self.cache_entries[key] = {'file': cache_file_name, 'version': version}
+                print "updated file in cache"
+                return
+            print "file is up to date - dont need to do anything..."
+        except Exception as e:
+            print "ERROR: occurred when adding an entry to the cache\n{}".format(e.message)
 
     def update_data_in_cache(self, cache_file_name, updated_data):
-        open_file = open(cache_file_name, 'w')
-        open_file.write(updated_data)
-        open_file.close()
-        self.set_version_of_file(cache_file_name)
+        try:
+            open_file = open(cache_file_name, 'w')
+            open_file.write(updated_data)
+            open_file.close()
+            self.set_version_of_file(cache_file_name)
+        except Exception as e:
+            print "ERROR: occurred when updating data in the cache\n{}".format(e.message)
 
     def replace_file_LRU_policy(self):
-        print "getting key to replace a file via LRU"
-        # find the last accessed value - i think this may be max not min!
-        key = find_LRU_key(self.cache_entries)
-        dic = self.cache_entries[key]
-        name = dic['file']
-        dic.pop('version', 0)
-        dic.pop('file', 0)
-        file_to_delete_from_cache = 'Cache{}/{}.txt'.format(self.client_id, name)
-        os.remove(file_to_delete_from_cache)
-        return key
+        try:
+            print "getting key to replace a file via LRU"
+            # find the last accessed value - i think this may be max not min!
+            key = find_LRU_key(self.cache_entries)
+            dic = self.cache_entries[key]
+            name = dic['file']
+            dic.pop('version', 0)
+            dic.pop('file', 0)
+            file_to_delete_from_cache = 'Cache{}/{}.txt'.format(self.client_id, name)
+            os.remove(file_to_delete_from_cache)
+            return key
+        except Exception as e:
+            print "ERROR: occurred when replacing a file in the cache\n{}".format(e.message)
 
     def empty_out_the_cache(self):
-        print "emptying out the cache"
-        self.cache_entries = {}
+        try:
+            print "emptying out the cache"
+            self.cache_entries = {}
+        except Exception as e:
+            print "ERROR occurred when clearing the cache\n{}".format(e.message)
 
     def get_cache_entry(self, cache_file_name):
-        print "getting entry from cache"
-        key = self.get_key_to_file(cache_file_name)
-        if key is not None:
-            return self.data_from_cache(key)
-        else:
-            return None
+        try:
+            print "getting entry from cache"
+            key = self.get_key_to_file(cache_file_name)
+            if key is not None:
+                return self.data_from_cache(key)
+            else:
+                return None
+        except Exception as e:
+            print "ERROR: occurred when getting an entry from the cache\n{}".format(e.message)
 
     def get_key_to_file(self, cache_file_name):
-        if len(self.cache_entries) is 0:
-            return -1
-        for i in range(self.number_of_cache_entries):
-            if self.cache_entries[i]['file'] is cache_file_name:
-                return i
-        return 0
+        try:
+            if len(self.cache_entries) is 0:
+                return -1
+            for i in range(self.number_of_cache_entries):
+                if self.cache_entries[i]['file'] is cache_file_name:
+                    return i
+            return 0
+        except Exception as e:
+            print "ERROR: occurred when getting the key to a file\n{}".format(e.message)
 
     def get_version_of_file(self, cache_file_name):
-        for i in range(self.number_of_cache_entries):
-            if self.cache_entries[i]['file'] is cache_file_name:
-                return self.cache_entries[i]['version'], self.cache_entries[i]['file'], cache_file_name
-        return 0, 0, 0
+        try:
+            for i in range(self.number_of_cache_entries):
+                if self.cache_entries[i]['file'] is cache_file_name:
+                    return self.cache_entries[i]['version'], self.cache_entries[i]['file'], cache_file_name
+            return 0, 0, 0
+        except Exception as e:
+            print "ERROR: occurred when getting version for file: {}\n{}".format(cache_file_name, e.message)
 
     def data_from_cache(self, key):
-        f = open(self.cache_entries[key]['file'], 'r')
-        data = f.read()
-        return {'file': self.cache_entries[key]['file'], 'version': self.cache_entries[key]['version'],
-                'data': data}
+        try:
+            f = open(self.cache_entries[key]['file'], 'r')
+            data = f.read()
+            return {'file': self.cache_entries[key]['file'], 'version': self.cache_entries[key]['version'],
+                    'data': data}
+        except Exception as e:
+            print "ERROR: error occurred when retreiving data from cache\n{}".format(e.message)
 
     def set_version_of_file(self, cache_file_name):
-        print str(self.cache_entries)
-        for i in range(self.number_of_cache_entries):
-            if self.cache_entries[i]['file'] is cache_file_name:
-                # the new version is this time now!
-                self.cache_entries[i]['version'] = str(datetime.datetime.now())
+        try:
+            print str(self.cache_entries)
+            for i in range(self.number_of_cache_entries):
+                if self.cache_entries[i]['file'] is cache_file_name:
+                    # the new version is this time now!
+                    self.cache_entries[i]['version'] = str(datetime.datetime.now())
+        except Exception as e:
+            print "ERROR: occurred when setting version for file: {}\n{}".format(cache_file_name, e.message)
 
 
 if __name__ == "__main__":
--- Client/ClientApi.py
+++ Client/ClientApi.py
@@ -11,136 +11,149 @@ CACHE_DIR = ''
 
 
 def get_client_num():
-    path = os.getcwd()
-    response_from_create_client_id = requests.get(
-        Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1], 'create_new_client'),
-        json={'client_id': 'Y', 'path': path}
-    )
-    if response_from_create_client_id is not None:
-        return response_from_create_client_id.json()['client_id']
+    try:
+        path = os.getcwd()
+        response_from_create_client_id = requests.get(
+            Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1], 'create_new_client'),
+            json={'client_id': 'Y', 'path': path}
+        )
+        if response_from_create_client_id is not None:
+            return response_from_create_client_id.json()['client_id']
+    except Exception as e:
+        print "ERROR: error occured when getting client id\n{}".format(e.message)
 
 
 def read_file_from_server(file_name, cache):
-    # ask directory_server for the current version number for the file
-    # check that against my cache - if the same, read from cache, else update cache and read from server
-    file_to_read = "Cache{}/".format(cache.client_id) + file_name
+    try:
+        file_to_read = "Cache{}/".format(cache.client_id) + file_name
 
-    response_from_directory_server = requests.get(
-        Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1]),
-        params={'file_name': file_name}
-    )
+        response_from_directory_server = requests.get(
+            Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1]),
+            params={'file_name': file_name}
+        )
 
-    # update cache if its needed
-    if cache.get_key_to_file(file_name) is None or response_from_directory_server.json()['version'] > \
-            cache.get_version_of_file(file_name):
-        file_server_details = response_from_directory_server.json()['file_server_details']
-        file_id = response_from_directory_server.json()['file_id']
-        file_server_id = response_from_directory_server.json()['file_server_id']
-        # check and see if the file details are up to date - if they are ignore, else update cache
-        if file_server_details is not None and file_id is not None:
-            response_from_directory_server = requests.get(
-                Fsl.create_url(file_server_details[0], file_server_details[1]),
-                params={'file_id': file_id, 'file_server_id': file_server_id}
-            )
-            print response_from_directory_server.json()
+        # update cache if its needed
+        if cache.get_key_to_file(file_name) is None or response_from_directory_server.json()['version'] > \
+                cache.get_version_of_file(file_name):
+            file_server_details = response_from_directory_server.json()['file_server_details']
+            file_id = response_from_directory_server.json()['file_id']
+            file_server_id = response_from_directory_server.json()['file_server_id']
+            # check and see if the file details are up to date - if they are ignore, else update cache
+            if file_server_details is not None and file_id is not None:
+                response_from_directory_server = requests.get(
+                    Fsl.create_url(file_server_details[0], file_server_details[1]),
+                    params={'file_id': file_id, 'file_server_id': file_server_id}
+                )
+                print response_from_directory_server.json()
 
-    open_file = open(file_to_read + '.txt', 'w')
-    open_file.close()
-    os.system('gedit "{0}"'.format(file_to_read + '.txt'))
-    data_to_cache = open(file_to_read + '.txt', 'r').read()
-    cache.add_cache_entry(file_name, cache.get_version_of_file(file_name), data_to_cache)
+        open_file = open(file_to_read + '.txt', 'w')
+        open_file.close()
+        os.system('gedit "{0}"'.format(file_to_read + '.txt'))
+        data_to_cache = open(file_to_read + '.txt', 'r').read()
+        cache.add_cache_entry(file_name, cache.get_version_of_file(file_name), data_to_cache)
+    except Exception as e:
+        print "ERROR: occurred when reading file from server:{}".format(e.message)
 
 
-# TODO - I need to fix this for handling versioning
 def write_file_to_server(file_name, cache):
-    key_in_cache = cache.get_key_to_file(file_name)
-    version, cache_f, f = cache.get_version_of_file(file_name)
-    print "cache_f {}: f{}".format(cache_f, f)
-    if key_in_cache is not None:
-        # I have a file in the cache
+    try:
+        key_in_cache = cache.get_key_to_file(file_name)
+        version, cache_f, f = cache.get_version_of_file(file_name)
+        print "cache_f {}: f{}".format(cache_f, f)
+        if key_in_cache is not None:
+            # I have a file in the cache
+            response_from_directory_server = requests.get(
+                Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1]),
+                params={'file_name': file_name}
+            )
+            file_server_details = response_from_directory_server.json()['file_server_details']
+            file_id = response_from_directory_server.json()['file_id']
+            file_server_id = response_from_directory_server.json()['file_server_id']
+
+            # get details for the server my file is on
+            print "Trying to obtain a lock on file {} for client {}...".format(file_name, cache.client_id)
+            # try to lokc file on server
+            response_from_locking_server = requests.get(
+                Fsl.create_url(LOCKING_SERVER_DETAILS[0], LOCKING_SERVER_DETAILS[1]),
+                json={'client_id': cache.client_id, 'file_id': file_id, 'file_server_id': file_server_id}
+            )
+
+            # if I can lock this file...
+            if response_from_locking_server.json()['lock']:
+                file_to_read = "Cache{}/".format(cache.client_id) + file_name
+
+                os.system('gedit "{0}"'.format(file_to_read + '.txt'))  # last chance to edit file before synch w/ server
+                data_to_send = open(file_to_read + '.txt', 'r').read()
+                print "http {}\nport {}".format(file_server_details[0], file_server_details[1])
+                response = requests.post(
+                    Fsl.create_url(file_server_details[0], file_server_details[1]),
+                    json={'file_id': file_id, 'data': data_to_send, 'server_id': file_server_id, 'file_name': file_name,
+                          'version': str(datetime.datetime.now())}
+
+                )
+                print response.json()
+                print "unlocking client{} from file .\n".format(cache.client_id, file_id)
+                response_from_locking_server = requests.delete(
+                    Fsl.create_url(LOCKING_SERVER_DETAILS[0], LOCKING_SERVER_DETAILS[1]),
+                    json={'client_id': cache.client_id, 'file_id': file_id,
+                          'file_server_id': file_server_id}
+                )
+                print "unlock response: {}".format(response_from_locking_server)
+
+            elif not response_from_locking_server.json()['lock']:
+                # TODO - I want to try implement a Queue here later on
+                print 'The file is locked on another - Try again later...'
+        else:
+            print "Data is not in cache/stored locally!"
+    except Exception as e:
+        print "ERROR: occurred when writing files to the server\n{}".format(e.message)
+
+
+def verify_file_exists(file_name, cache):
+    try:
         response_from_directory_server = requests.get(
             Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1]),
             params={'file_name': file_name}
         )
         file_server_details = response_from_directory_server.json()['file_server_details']
         file_id = response_from_directory_server.json()['file_id']
-        file_server_id = response_from_directory_server.json()['file_server_id']
 
-        # get details for the server my file is on
-        print "Trying to obtain a lock on file {} for client {}...".format(file_name, cache.client_id)
-        # try to lokc file on server
-        response_from_locking_server = requests.get(
-            Fsl.create_url(LOCKING_SERVER_DETAILS[0], LOCKING_SERVER_DETAILS[1]),
-            json={'client_id': cache.client_id, 'file_id': file_id, 'file_server_id': file_server_id}
-        )
-
-        # if I can lock this file...
-        if response_from_locking_server.json()['lock']:
-            file_to_read = "Cache{}/".format(cache.client_id) + file_name
-
-            os.system('gedit "{0}"'.format(file_to_read + '.txt'))  # last chance to edit file before synch w/ server
-            data_to_send = open(file_to_read + '.txt', 'r').read()
-            print "http {}\nport {}".format(file_server_details[0], file_server_details[1])
-            response = requests.post(
-                Fsl.create_url(file_server_details[0], file_server_details[1]),
-                json={'file_id': file_id, 'data': data_to_send, 'server_id': file_server_id, 'file_name': file_name,
-                      'version': str(datetime.datetime.now())}
-
-            )
-            print response.json()
-            print "unlocking client{} from file .\n".format(cache.client_id, file_id)
-            response_from_locking_server = requests.delete(
-                Fsl.create_url(LOCKING_SERVER_DETAILS[0], LOCKING_SERVER_DETAILS[1]),
-                json={'client_id': cache.client_id, 'file_id': file_id,
-                      'file_server_id': file_server_id}
-            )
-            print "unlock response: {}".format(response_from_locking_server)
-
-        elif not response_from_locking_server.json()['lock']:
-            # TODO - I want to try implement a Queue here later on
-            print 'The file is locked on another - Try again later...'
-    else:
-        print "Data is not in cache/stored locally!"
-
-
-def verify_file_exists(file_name, cache):
-    response_from_directory_server = requests.get(
-        Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1]),
-        params={'file_name': file_name}
-    )
-    file_server_details = response_from_directory_server.json()['file_server_details']
-    file_id = response_from_directory_server.json()['file_id']
-
-    if file_server_details is not None:
-        print "file {0} requested by client {1} is on the file server: 'http://{2}:{3}\nAnd has id:{4}\n". \
-            format(file_name, cache.client_id, file_server_details[0], file_server_details[1], file_id)
-    elif cache.get_key_to_file(file_name) is not None:
-        print "file {0} requested by client {1} is in your cache\n". \
-            format(file_name, cache.client_id)
-    else:
-        print "The file {0} requested by client{1} does not exist on any of our servers...\n".format(file_name,
-                                                                                                     cache.client_id)
-    return file_server_details, file_id
+        if file_server_details is not None:
+            print "file {0} requested by client {1} is on the file server: 'http://{2}:{3}\nAnd has id:{4}\n". \
+                format(file_name, cache.client_id, file_server_details[0], file_server_details[1], file_id)
+        elif cache.get_key_to_file(file_name) is not None:
+            print "file {0} requested by client {1} is in your cache\n". \
+                format(file_name, cache.client_id)
+        else:
+            print "The file {0} requested by client{1} does not exist on any of our servers...\n".format(file_name,
+                                                                                                         cache.client_id)
+        return file_server_details, file_id
+    except Exception as e:
+        print "ERROR: occurred when verifying if file exists\n{}".format(e.message)
 
 
 def create_cache_for_client(client_id, path):
-    cache = Cache.Cache(client_id)
-    cache.initialise_cache(path, client_id)
-    print "new cache set up for client{}".format(client_id)
-    return cache
+    try:
+        cache = Cache.Cache(client_id)
+        cache.initialise_cache(path, client_id)
+        print "new cache set up for client{}".format(client_id)
+        return cache
+    except Exception as e:
+        print "ERROR: occureed when creating a cache for the client\n{}".format(e.message)
 
 
 def create_new_file(file_name, cache):
-    print "Creating a new file {0} for the client{1}\n".format(file_name, cache.client_id)
-    #f = open('Cache' + str(cache.client_id) + '/' + file_name, 'w')
-    #f.close()
-    request_to_server = {'file_name': file_name, 'version': str(datetime.datetime.now())}
-    response = requests.post(
-        Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1]),
-        params=request_to_server
-    )
-    if response.json()['message'] is True:
-        print "file {0} has been created for client{1}".format(file_name, cache.client_id)
+    try:
+        print "Creating a new file {0} for the client{1}\n".format(file_name, cache.client_id)
+        request_to_server = {'file_name': file_name, 'version': str(datetime.datetime.now())}
+        response = requests.post(
+            Fsl.create_url(DIRECTORY_SERVER_DETAILS[0], DIRECTORY_SERVER_DETAILS[1]),
+            params=request_to_server
+        )
+        if response.json()['message'] is True:
+            print "file {0} has been created for client{1}".format(file_name, cache.client_id)
+    except Exception as e:
+        print "ERROR: occurred when creating a new file\n{}".format(e.message)
 
 
 def handle_client_request(client_req, client_id, cache):
@@ -180,7 +193,3 @@ def handle_client_request(client_req, client_id, cache):
         print "client{} requested to leave...".format(client_id)
         return False
     return True
-
-
-def print_(message):
-    print message
